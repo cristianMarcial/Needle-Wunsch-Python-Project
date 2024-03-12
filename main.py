316be66f8f8918aa62e7d2c4a2b4c717140d6f0c
cristianMacial <cristian.marcial@upr.edu>
2024-03-12 17:38:12 -0400

    c

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-#x
+import csv
+
+print('Inserte \n')
+
+try:
+   input = input() #input = 'C:/Users/Admin/Desktop/excel_prueba1.csv'
+except:
+  print("An exception occurred")
+
+
+with open(input, newline='') as file:
+  csvFile = csv.reader(file)
+  for lines in csvFile:
+        print(lines)
