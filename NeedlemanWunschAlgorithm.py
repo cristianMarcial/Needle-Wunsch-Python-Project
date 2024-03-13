69a96b46e2e1bb997f6c53a00a70f1d8d5a06214
cristianMacial <cristian.marcial@upr.edu>
2024-03-13 18:32:26 -0400

    xd

--- NeedlemanWunschAlgorithm.py
+++ NeedlemanWunschAlgorithm.py
@@ -3,15 +3,38 @@ MATCH_SCORE = 1
 MISMATCH_SCORE = -1
 GAP_SCORE = -2
 
-def S(a, b):
-    return 1 if a > b else -1
+grid = []
 
-def max():
-    pass
+def S(a, b):
+    return MATCH_SCORE if a == b else MISMATCH_SCORE
+
+def generateGrid(s1, s2):
+    #Initialization step
+    firstRow = [0]
+
+    index = 1
+    for i in s1:
+        firstRow.append(GAP_SCORE*index)
+        index+=1
+    grid.append(firstRow)
+
+    index = 1
+    for j in s2:
+        grid.append([GAP_SCORE*index])
+        index+=1
+
+    #Filling the grid
+    
+    index = 1
+    for j in s2:
+        for i in s1:
+            grid[index].append(max(5,7))
 
 def printOutput(inputText):
-    s1 = ''
-    s2 = ''
+    s1 = inputText[0]
+    s2 = inputText[1]
     score = 0
 
-    print(s1, s2, score)
+    generateGrid(s1,s2)
+
+    print(grid)
--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 import csv
 import NeedlemanWunschAlgorithm as nwa
 
-try:
-    input = input('Insert the file path: ') #input = 'C:/Users/Admin/Desktop/excel_prueba1.csv'
+#try:
+input = input('Insert the file path: ') #input = 'C:/Users/Admin/Desktop/excel_prueba1.csv'
 
-    with open(input, newline='') as file:
-        #This makes sure that the lines are read as an csv file.
-        csvFile = csv.reader(file)
+with open(input, newline='') as file:
+    #This makes sure that the lines are read as an csv file.
+    csvFile = csv.reader(file)
 
-        #This omits the first row on the csv document
-        file.readline()
+    #This omits the first row on the csv document
+    file.readline()
 
-        for lines in csvFile:
-            nwa.printOutput(lines[1])
-    file.close()
+    for lines in csvFile:
+        nwa.printOutput(lines[1])
+file.close()
 
-except:
-    print('An exception occurred while opening the file. Recompile this program again and make sure the input is well written')
+#except:
+#    print('An exception occurred while opening the file. Recompile this program again and make sure the input is well written')
