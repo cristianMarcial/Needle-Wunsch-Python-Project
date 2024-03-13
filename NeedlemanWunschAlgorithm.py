f92c973eb87639d35f0dd6df737fde3e6df887f9
cristianMacial <cristian.marcial@upr.edu>
2024-03-12 21:43:58 -0400

    XDDDD

--- NeedlemanWunschAlgorithm.py
+++ NeedlemanWunschAlgorithm.py
@@ -1,6 +1,17 @@
-#lmao
+
+MATCH_SCORE = 1
+MISMATCH_SCORE = -1
+GAP_SCORE = -2
+
+def S(a, b):
+    return 1 if a > b else -1
+
+def max():
+    pass
 
 def printOutput(inputText):
-    val = 0
+    s1 = ''
+    s2 = ''
+    score = 0
 
-    print(inputText)
+    print(s1, s2, score)
--- main.py
+++ main.py
@@ -8,12 +8,12 @@ try:
         #This makes sure that the lines are read as an csv file.
         csvFile = csv.reader(file)
 
-        #This omits the first line on the doc, which has ...
+        #This omits the first row on the csv document
         file.readline()
-        
+
         for lines in csvFile:
             nwa.printOutput(lines[1])
     file.close()
 
 except:
-    print("An exception occurred while opening the file. Recompile this program again and make sure the input is well written")
+    print('An exception occurred while opening the file. Recompile this program again and make sure the input is well written')
