69ffa1273ebfbe5ee89288e0a62b9751600e3089
cristianMacial <cristian.marcial@upr.edu>
2024-03-13 23:18:11 -0400

    xd

--- NeedlemanWunschAlgorithm.py
+++ NeedlemanWunschAlgorithm.py
@@ -1,5 +1,4 @@
-
-#Scoring matrix values
+#Scoring matrix values [[0, -2, -4, -6, -8, -10], [-2, 1, -1, -3, -5, -7], [-4, -1, 0, 0, -2, -4], [-6, -3, -2, -1, 1, -1], [-8, -5, -2, -3, -1, 2]]
 MATCH_SCORE = 1
 MISMATCH_SCORE = -1
 GAP_SCORE = -2
@@ -8,22 +7,20 @@ def S(a, b):
     return MATCH_SCORE if a == b else MISMATCH_SCORE
 
 def generateGrid(s1, s2):
-    grid = []
+    grid = [[0]]
 
     #Initialization step
-    firstRow = [0]
     index = 1
     for i in s1:
-        firstRow.append(GAP_SCORE*index)
+        grid[0].append(GAP_SCORE*index)
         index+=1
-    grid.append(firstRow)
 
     index = 1
     for j in s2:
         grid.append([GAP_SCORE*index])
         index+=1
 
-    #Filling the grid [[0, -2, -4, -6, -8, -10], [-2, 1, -1, -3, -5, -7], [-4, -1, 0, 0, -2, -4], [-6, -3, -2, -1, 1, -1], [-8, -5, -2, -3, -1, 2]]
+    #Filling the grid
     y = 0
     for j in s2:
         x = 0
@@ -31,14 +28,16 @@ def generateGrid(s1, s2):
         for i in s1:
             x+=1
             grid[y].append(max(grid[y-1][x-1] + S(i,j), grid[y-1][x] + GAP_SCORE, grid[y][x-1] + GAP_SCORE))
+    
     return grid
 
 def printOutput(inputText):
     s1 = inputText[0]
     s2 = inputText[1]
-    score = 0
+    grid = generateGrid(s1,s2)
+    score = grid[-1][-1]
 
     #
-    print(generateGrid(s1,s2))
-    
+    print(grid)
+
     #print(s2)
