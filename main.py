f8b0575c41f316059be57ed23a37ca676c6433b3
cristianMacial <cristian.marcial@upr.edu>
2024-03-28 21:21:40 -0400

    Fixed.

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
-import csv, sys
-import NeedlemanWunschAlgorithm as nwa
+import csv, sys, NeedlemanWunschAlgorithm as nwa
 
-#Exampleof an input: py main.py test1.csv
+# Example of an valid input: py main.py test1.csv
 
-if(len(sys.argv) > 1):
-    with open(sys.argv[1]) as file:
-        # This makes sure that the lines are read as an csv file.
-        csvFile = csv.reader(file)
+try:
+    if(len(sys.argv) > 1):
+        with open(sys.argv[1]) as file:
+            # This makes sure that the lines are read as an csv file.
+            csvFile = csv.reader(file)
 
-        # This omits the first row on the csv document
-        file.readline()
+            # This omits the first row on the csv document.
+            file.readline()
 
-        for lines in csvFile: 
-            nwa.printOutput(lines)
-    file.close()
-else:
-    print("Invalid input; this only works with a .csv file as an input on the command line")
+            for lines in csvFile: 
+                nwa.printOutput(lines)
+        file.close()
+    else:
+        print("Invalid input; this only works with a .csv file as an input on the command line.")
+except:
+    print('An error has ocurred while reading the previous input.')
